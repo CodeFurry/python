15074d48b66bb6e296e34c615c1c3dabe60ea943
CodeFurry <168342014+CodeFurry@users.noreply.github.com>
2024-06-23 06:42:04 +0530

    Update importer.py
    added return types

--- importer.py
+++ importer.py
@@ -4,25 +4,25 @@ pygame.init()
 # please consider giving feedback
 
 class imageCutter:
-	def __init__(self, base_path: str, sheet: str | pygame.surface.Surface):
+	def __init__(self, base_path: str, sheet: str | pygame.surface.Surface) -> None:
 		''' defines a sheet or image as the spritesheet used in the whole program '''
     		if isinstance(sheet, str):
 			self.sheet = pygame.image.load(f"{base_path}/{sheet}")
 		else: self.sheet = sheet
 
-	def cutImages(self, width: int, height: int, frame: int):
+	def cutImages(self, width: int, height: int, frame: int) -> pg.surface.Surface:
     		''' cut images from a sprite sheet horizontally in squares '''
 		surface = pygame.Surface((width, height), pygame.SRCALPHA)
 		surface.blit(self.sheet, (0, 0), (width * frame, 0, width, height))
 		return surface
 
-	def cutTilesUndefined(self, width: int, height: int, start_hor: int, start_ver: int):
+	def cutTilesUndefined(self, width: int, height: int, start_hor: int, start_ver: int) -> pg.surface.Surface:
     		''' cut image from a spritesheet from any part of the sheet '''
 		surface = pygame.Surface((width, height), pygame.SRCALPHA)
 		surface.blit(self.sheet, (0, 0), (start_hor, start_ver, width, height))
 		return surface
 
-	def cutRandomTiles(self, width: int, height: int, start_hor: int, start_ver: int, scale: tuple = None):
+	def cutRandomTiles(self, width: int, height: int, start_hor: int, start_ver: int, scale: tuple = None) -> pg.surface.Surface:
     		''' works exactly like cutTilesUndefined function but with resizing, not useful at all '''
 		surface = pygame.Surface((width, height), pygame.SRCALPHA)
 		surface.blit(self.sheet, (0, 0), (start_hor, start_ver, width, height))
